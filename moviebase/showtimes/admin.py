8b34a681934e8fd3db56986fc9c2550b952d6458
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-03 21:19:32 +0100

    register "Cinema" and "Screening" models

--- moviebase/showtimes/admin.py
+++ moviebase/showtimes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Cinema,
+    Screening
+)
 
-# Register your models here.
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Screening)
+class ScreeningAdmin(admin.ModelAdmin):
+    pass
