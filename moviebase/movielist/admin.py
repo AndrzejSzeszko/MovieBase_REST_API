90468f3ff39c2648fbd1f1ea005e36b0d6016ca1
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-03 21:19:17 +0100

    register "Person" and "Movie" models

--- moviebase/movielist/admin.py
+++ moviebase/movielist/admin.py
@@ -1,2 +1,15 @@
 from django.contrib import admin
-from .models import Movie
+from .models import (
+    Movie,
+    Person
+)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
