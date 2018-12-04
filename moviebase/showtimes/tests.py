394d4f02b61ee4ad6dffd95733ed4aa2a323ff7d
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-04 21:52:33 +0100

    create "setUpClass" method and auxiliary methods for creating instances of all models

--- moviebase/showtimes/tests.py
+++ moviebase/showtimes/tests.py
@@ -14,45 +14,66 @@ from movielist.models import (
 
 
 class ShowtimesTestCase(APITestCase):
-
-    def setUp(self):
-        fake = Faker('pl-PL')
+    fake = Faker('pl-PL')
+    
+    @classmethod
+    def setUpClass(cls):
 
         for _ in range(4):
-            Person.objects.create(name=fake.name())
+            Person.objects.create(**cls._fake_person_data())
 
-        available_people = Person.objects.all()
+        cls.available_people = Person.objects.all()
         for _ in range(3):
-            new_movie = Movie.objects.create(
-                title=fake.text(20)[:-1],
-                description=fake.text(100),
-                director=random.choice(available_people),
-                year=random.randint(1900, 2019),
-            )
-            new_movie.actors.set(random.sample(available_people, 3))
+            new_movie = Movie.objects.create(**cls._fake_movie_data())
+            new_movie.actors.set(random.sample([person for person in cls.available_people], 3))
 
         for _ in range(3):
-            Cinema.objects.create(
-                name=fake.company(),
-                city=fake.city()
-            )
+            Cinema.objects.create(**cls._fake_cinema_data())
 
         for cinema in Cinema.objects.all():
             for _ in range(5):
-                Screening.objects.create(
-                    cinema=cinema,
-                    movie=random.choice(Movie.objects.all()),
-                    date=datetime(
-                        random.randint(2018, 2019),
-                        random.randint(1, 12),
-                        random.randint(0, 23),
-                        random.randint(0, 59)
-                    )
-                )
+                Screening.objects.create(**cls._fake_screening_data(cinema))
 
+    @classmethod
+    def _fake_person_data(cls):
+        return dict(
+            name=cls.fake.name()
+        )
+
+    @classmethod
+    def _fake_movie_data(cls):
+        return dict(
+            title=cls.fake.text(20)[:-1],
+            description=cls.fake.text(100),
+            director=random.choice(cls.available_people),
+            year=random.randint(1900, 2019),
+        )
+
+    @classmethod
+    def _fake_cinema_data(cls):
+        return dict(
+           name=cls.fake.company(),
+           city=cls.fake.city()
+        )
+
+    @classmethod
+    def _fake_screening_data(cls, cinema):
+        return dict(
+            cinema=cinema,
+            movie=random.choice(Movie.objects.all()),
+            date=datetime(
+                random.randint(2018, 2019),
+                random.randint(1, 12),
+                random.randint(1, 28),
+                random.randint(0, 23),
+                random.randint(0, 59)
+                )
+            )
 
     def test_add_cinema(self):
-        pass
+        new_cinema_data = self._fake_cinema_data()
+        response = self.client.post('')
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         pass
