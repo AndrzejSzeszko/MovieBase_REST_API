40f9c14a309e208c7c9dcf0977d967984720e43c
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-05 21:31:17 +0100

    add "screening_room" field and unique_together in Screening

--- moviebase/showtimes/models.py
+++ moviebase/showtimes/models.py
@@ -12,9 +12,13 @@ class Cinema(models.Model):
 
 
 class Screening(models.Model):
-    cinema = models.ForeignKey(Cinema, on_delete=models.SET_NULL, null=True)
-    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True)
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    screening_room = models.CharField(max_length=64, null=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     date = models.DateTimeField()
 
     def __str__(self):
         return f'{self.cinema}-{self.movie.title}-{self.date}'
+
+    class Meta:
+        unique_together = ['cinema', 'screening_room', 'date']
