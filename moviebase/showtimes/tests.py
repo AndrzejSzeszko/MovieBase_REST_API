264acc3dd020d1e351d3c965465aeb6fa1b3a7f2
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-06 15:24:26 +0100

    make tests for Screening model

--- moviebase/showtimes/tests.py
+++ moviebase/showtimes/tests.py
@@ -32,7 +32,11 @@ class ShowtimesTestCase(APITestCase):
 
         for cinema in Cinema.objects.all():
             for _ in range(5):
-                Screening.objects.create(**cls._fake_screening_data(cinema))
+                Screening.objects.create(**cls._fake_screening_data(
+                    cinema=cinema,
+                    movie=random.choice(Movie.objects.all())
+                    )
+                )
 
     @classmethod
     def _fake_person_data(cls):
@@ -57,10 +61,11 @@ class ShowtimesTestCase(APITestCase):
         )
 
     @classmethod
-    def _fake_screening_data(cls, cinema):
+    def _fake_screening_data(cls, cinema, movie):
         return dict(
             cinema=cinema,
-            movie=random.choice(Movie.objects.all()),
+            movie=movie,
+            screening_room=random.randint(1, 34),
             date=datetime(
                 random.randint(2018, 2019),
                 random.randint(1, 12),
@@ -70,11 +75,11 @@ class ShowtimesTestCase(APITestCase):
                 )
             )
 
-    def test1_list_cinemas(self):
+    def test_a_list_cinemas(self):
         response = self.client.get('/cinemas/', {}, format='json')
         self.assertEqual(response.status_code, 200)
 
-    def test2_cinema_details(self):
+    def test_b_cinema_details(self):
         response = self.client.get('/cinemas/1/', {}, format='json')
         self.assertEqual(response.status_code, 200)
 
@@ -84,13 +89,13 @@ class ShowtimesTestCase(APITestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Cinema.objects.filter(**new_cinema_data).count(), 1)
 
-    def test4_update_cinema(self):
+    def test_c_update_cinema(self):
         updated_cinema_data = self._fake_cinema_data()
         response = self.client.patch('/cinemas/1/', updated_cinema_data, format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Cinema.objects.filter(**updated_cinema_data).count(), 1)
 
-    def test5_update_non_existing_cinema(self):
+    def test_d_update_non_existing_cinema(self):
         updated_cinema_data = self._fake_cinema_data()
         response = self.client.patch('/cinemas/9999999999999999999999999999/', updated_cinema_data, format='json')
         self.assertEqual(response.status_code, 404)
@@ -100,9 +105,74 @@ class ShowtimesTestCase(APITestCase):
         self.assertEqual(response.status_code, 204)
         self.assertEqual(Cinema.objects.filter(pk=1).count(), 0)
 
-    def test7_delete_non_existing_cinema(self):
+    def test_e_delete_non_existing_cinema(self):
         response = self.client.delete('/cinemas/9999999999999999999999999999/', {}, format='json')
         self.assertEqual(response.status_code, 404)
+
+    def test_f_list_screenings(self):
+        response = self.client.get('/screenings/', {}, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_g_screening_details(self):
+        response = self.client.get('/screenings/1/', {}, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_h_add_screening(self):
+        new_screening_data = self._fake_screening_data(
+            cinema=random.choice(Cinema.objects.all()),
+            movie=random.choice(Movie.objects.all())
+        )
+        new_screening_json_data = dict(new_screening_data)
+        new_screening_json_data.update(
+            {
+                'cinema': new_screening_data['cinema'].name,
+                'movie': new_screening_data['movie'].title
+            }
+        )
+        response = self.client.post('/screenings/', new_screening_json_data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Screening.objects.filter(**new_screening_data).count(), 1)
+
+    def test_i_update_screening(self):
+        updated_screening_data = self._fake_screening_data(
+            cinema=random.choice(Cinema.objects.all()),
+            movie=random.choice(Movie.objects.all())
+        )
+        updated_screening_json_data = dict(updated_screening_data)
+        updated_screening_json_data.update(
+            {
+                'cinema': updated_screening_data['cinema'].name,
+                'movie': updated_screening_data['movie'].title
+            }
+        )
+        response = self.client.patch('/screenings/1/', updated_screening_json_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Screening.objects.filter(**updated_screening_data).count(), 1)
+
+    def test_j_update_non_existing_screening(self):
+        updated_screening_data = self._fake_screening_data(
+            cinema=random.choice(Cinema.objects.all()),
+            movie=random.choice(Movie.objects.all())
+        )
+        updated_screening_json_data = dict(updated_screening_data)
+        updated_screening_json_data.update(
+            {
+                'cinema': updated_screening_data['cinema'].name,
+                'movie': updated_screening_data['movie'].title
+            }
+        )
+        response = self.client.patch('/screenings/9999999999999999999999999999/', updated_screening_json_data, format='json')
+        self.assertEqual(response.status_code, 404)
+
+    def test_k_delete_screening(self):
+        response = self.client.delete('/screenings/1/', {}, format='json')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Screening.objects.filter(pk=1).count(), 0)
+
+    def test_l_delete_non_existing_screening(self):
+        response = self.client.delete('/screenings/9999999999999999999999999999/', {}, format='json')
+        self.assertEqual(response.status_code, 404)
+
 
     @classmethod
     def tearDownClass(cls):
